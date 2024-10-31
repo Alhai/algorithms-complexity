7f950307d62acfaadf46015c4a393f9f2dbe79fc
Alhai <aly.haidara@seira-formation.fr>
2024-10-31 12:23:28 +0100

    WIP - display array

---
+++ tp-1/sorting-algorithms.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void displayArray(int array[], int size){
+    for(int i = 0; i < size; i++){
+        printf("%d", array[i]);
+    }
+    printf("%d\n");
+}
+
+void main(){
+ return 0;
+}
