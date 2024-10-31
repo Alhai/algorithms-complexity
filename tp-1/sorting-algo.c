96770be59523a89112161c49847e27c1ff897726
Alhai <aly.haidara@seira-formation.fr>
2024-10-31 14:10:23 +0100

    add bubbleSort

--- tp-1/sorting-algo.c
+++ tp-1/sorting-algo.c
@@ -7,6 +7,28 @@ void displayArray(int array[], int size){
     printf("%d\n");
 }
 
+void bubbleSort(int array[], int size){
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j - size - 1;j++){
+            if(array[i] > array[j + 1]){
+                int tempValue = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tempValue;
+            }
+        }
+    }
+}
 void main(){
- return 0;
+    int array[] = {66, 33, 11, 22, 44};
+    int size = sizeof(array) / sizeof(array[0]);
+
+    printf("Tableau original :\n");
+    afficherTableau(array, size);
+
+    int array1[] = {66, 33, 11, 22, 44};
+    triABulles(array1, size);
+    printf("\nTableau trié avec tri à bulles :\n");
+    afficherTableau(array1, size);
+
+return 0;
 }
